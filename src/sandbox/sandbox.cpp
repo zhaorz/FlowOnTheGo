8d013c57a9b35b703d98dbb817840f0b2a5792fc
Richard Zhao <richardz@andrew.cmu.edu>
2017-04-28 01:49:36 -0400

    Refactor sandbox into 'process' function

--- src/sandbox/sandbox.cpp
+++ src/sandbox/sandbox.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <iostream>
 #include <chrono>
+#include <string>
 #include <math.h>
 
 // OpenCV
@@ -49,8 +50,8 @@ static std::chrono::time_point<Clock> now() {
 
 double time_diff(std::chrono::time_point<Clock> start, std::chrono::time_point<Clock> end) {
   auto dt = end - start;
-  auto us = std::chrono::duration_cast<std::chrono::milliseconds>(dt);
-  return us.count();
+  auto us = std::chrono::duration_cast<std::chrono::microseconds>(dt);
+  return us.count() / 1000.0;
 }
 
 double calc_print_elapsed(const char* name, std::chrono::time_point<Clock> start) {
@@ -61,120 +62,141 @@ double calc_print_elapsed(const char* name, std::chrono::time_point<Clock> start
 
 void print_usage() {
   std::cout << "Usage: ./sandbox <input_img> <output_img>" << std::endl;
+  std::cout << "       ./sandbox <img1> <img2> ...  <output_prefix>" << std::endl;
+}
+
+void process(const char* input_file, const char* output_file) {
+  cv::Mat I0, I0_f;
+
+  // Record only relevant times
+  double compute_time = 0.0;
+
+  auto start_read = now();
+
+  // Get input
+  I0 = cv::imread(input_file, cv::IMREAD_GRAYSCALE);
+
+  calc_print_elapsed("imread", start_read);
+
+  // Check for invalid input
+  if(!I0.data) {
+    std::cout <<  "Could not open or find the image" << std::endl ;
+    exit(1);
+  }
+
+
+  auto start_convert = now();
+
+  // Convert to float
+  I0.convertTo(I0_f, CV_32F);
+
+  calc_print_elapsed("convertTo float", start_convert);
+
+
+  cv::Size sz = I0_f.size();
+
+  int width = sz.width;
+  int height = sz.height;
+
+  std::cout << "Processing " << width << "x" << height << " image" << std::endl;
+
+  // pSrc pointer to image data
+  Npp32f* pHostSrc = (float*) I0_f.data;
+
+  // The width, in bytes, of the image, sometimes referred to as pitch
+  unsigned int nSrcStep = width * sizeof(float);
+  unsigned int nDstStep = nSrcStep;
+
+  NppiSize oSizeROI = { width, height };
+
+
+  auto start_cuda_malloc = now();
+
+  // Allocate device memory
+  Npp32f* pDeviceSrc, *pDeviceDst;
+  checkCudaErrors( cudaMalloc((void**) &pDeviceSrc, width * height * sizeof(float)) );
+  checkCudaErrors( cudaMalloc((void**) &pDeviceDst, width * height * sizeof(float)) );
+
+  calc_print_elapsed("cudaMalloc", start_cuda_malloc);
+
+
+  auto start_memcpy_hd = now();
+
+  // Copy image to device
+  checkCudaErrors(
+      cudaMemcpy(pDeviceSrc, pHostSrc, width * height * sizeof(float), cudaMemcpyHostToDevice) );
+
+  calc_print_elapsed("cudaMemcpy H->D", start_memcpy_hd);
+
+
+  auto start_sobel = now();
+
+  NPP_CHECK_NPP(
+      nppiFilterSobelHoriz_32f_C1R (pDeviceSrc, nSrcStep, pDeviceDst, nDstStep, oSizeROI) );
+
+  compute_time += calc_print_elapsed("sobel", start_sobel);
+
+
+  auto start_memcpy_dh = now();
+
+
+  // Copy result to host, reuse the same pointer
+  float* pHostDst = new float[width * height * sizeof(float)];
+  checkCudaErrors(
+      cudaMemcpy(pHostDst, pDeviceDst, width * height * sizeof(float), cudaMemcpyDeviceToHost) );
+
+  calc_print_elapsed("cudaMemcpy H<-D", start_memcpy_dh);
+
+
+  auto start_mat_init = now();
+
+  // Init output image
+  cv::Mat I1(height, width, CV_32F, pHostDst);
+
+  calc_print_elapsed("cv mat init", start_mat_init);
+
+
+  auto start_write = now();
+
+  // Write output
+  cv::imwrite(output_file, I1);
+
+  calc_print_elapsed("write", start_write);
+
+  cudaFree((void*) pDeviceSrc);
+  cudaFree((void*) pDeviceDst);
+
+  delete[] pHostDst;
+
+  std::cout << "[complete] Primary compute time: " << compute_time << " (ms)" << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-  const char* input_file  = argv[1];
-  const char* output_file = argv[2];
-
-  initializeCuda(argc, argv);
-
   if (argc == 3) {
-    cv::Mat I0, I0_f;
+    initializeCuda(argc, argv);
 
-    // Record only relevant times
-    double compute_time = 0.0;
+    const char* input_file  = argv[1];
+    const char* output_file = argv[2];
 
-    auto start_read = now();
+    process(input_file, output_file);
+  }
 
-    // Get input
-    I0 = cv::imread(input_file, cv::IMREAD_GRAYSCALE);
+  else if (argc > 3) {
+    int num_inputs = argc - 2;
+    std::string output_prefix(argv[argc-1]);
 
-    calc_print_elapsed("imread", start_read);
+    for (int input_idx = 0; input_idx < num_inputs; input_idx++) {
+      const char* input_file  = argv[1 + input_idx];
+      std::string output_file = output_prefix + "_" + std::to_string(input_idx) + ".png";
 
-    // Check for invalid input
-    if(!I0.data) {
-      std::cout <<  "Could not open or find the image" << std::endl ;
-      return -1;
+      std::cout << "[" << input_idx + 1 << "/" << num_inputs << "] Processing "
+        << input_file << " => " << output_file << std::endl;
+
+      process(input_file, output_file.c_str());
+
+      std::cout << std::endl;
     }
-
-
-    auto start_convert = now();
-
-    // Convert to float
-    I0.convertTo(I0_f, CV_32F);
-
-    calc_print_elapsed("convertTo float", start_convert);
-
-
-    cv::Size sz = I0_f.size();
-
-    int width = sz.width;
-    int height = sz.height;
-
-    std::cout << "Processing " << width << "x" << height << " image" << std::endl;
-
-    // pSrc pointer to image data
-    Npp32f* pHostSrc = (float*) I0_f.data;
-
-    // The width, in bytes, of the image, sometimes referred to as pitch
-    unsigned int nSrcStep = width * sizeof(float);
-    unsigned int nDstStep = nSrcStep;
-
-    NppiSize oSizeROI = { width, height };
-
-
-    auto start_cuda_malloc = now();
-
-    // Allocate device memory
-    Npp32f* pDeviceSrc, *pDeviceDst;
-    checkCudaErrors( cudaMalloc((void**) &pDeviceSrc, width * height * sizeof(float)) );
-    checkCudaErrors( cudaMalloc((void**) &pDeviceDst, width * height * sizeof(float)) );
-
-    calc_print_elapsed("cudaMalloc", start_cuda_malloc);
-
-
-    auto start_memcpy_hd = now();
-
-    // Copy image to device
-    checkCudaErrors(
-        cudaMemcpy(pDeviceSrc, pHostSrc, width * height * sizeof(float), cudaMemcpyHostToDevice) );
-
-    calc_print_elapsed("cudaMemcpy H->D", start_memcpy_hd);
-
-
-    auto start_sobel = now();
-
-    NPP_CHECK_NPP(
-        nppiFilterSobelHoriz_32f_C1R (pDeviceSrc, nSrcStep, pDeviceDst, nDstStep, oSizeROI) );
-
-    compute_time += calc_print_elapsed("sobel", start_sobel);
-
-
-    auto start_memcpy_dh = now();
-
-    // Copy result to host, reuse the same pointer
-    float pHostDst[width * height * sizeof(float)];
-    checkCudaErrors(
-        cudaMemcpy(pHostDst, pDeviceDst, width * height * sizeof(float), cudaMemcpyDeviceToHost) );
-
-    calc_print_elapsed("cudaMemcpy H<-D", start_memcpy_dh);
-
-
-    auto start_mat_init = now();
-
-    // Init output image
-    cv::Mat I1(height, width, CV_32F, pHostDst);
-
-    calc_print_elapsed("cv mat init", start_mat_init);
-
-
-    auto start_write = now();
-
-    // Write output
-    cv::imwrite(output_file, I1);
-
-    calc_print_elapsed("write", start_write);
-
-    cudaFree((void*) pDeviceSrc);
-    cudaFree((void*) pDeviceDst);
-
-    std::cout << "Done." << std::endl;
-
-    std::cout << "Primary compute time: " << compute_time << " (ms)" << std::endl;
-
-    return 0;
   }
 
   else {
