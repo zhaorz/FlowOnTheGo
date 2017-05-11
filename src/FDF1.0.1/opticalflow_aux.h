6d3e5d3e2988f4f42ac1042c9e68051f19096fa1
Richard Zhao <richardz@andrew.cmu.edu>
2017-05-11 00:39:44 +0000

    Add gpu data term

--- src/FDF1.0.1/opticalflow_aux.h
+++ src/FDF1.0.1/opticalflow_aux.h
@@ -42,8 +42,6 @@ void compute_data(image_t *a11, image_t *a12, image_t *a22, image_t *b1, image_t
 void compute_data(image_t *a11, image_t *a12, image_t *a22, image_t *b1, image_t *b2, image_t *mask, image_t *wx, image_t *wy, image_t *du, image_t *dv, image_t *uu, image_t *vv, color_image_t *Ix, color_image_t *Iy, color_image_t *Iz, color_image_t *Ixx, color_image_t *Ixy, color_image_t *Iyy, color_image_t *Ixz, color_image_t *Iyz, const float half_delta_over3, const float half_beta, const float half_gamma_over3);
 #endif
 
-void gpu_compute_data(image_t *a11, image_t *a12, image_t *a22, image_t *b1, image_t *b2, image_t *mask, image_t *wx, image_t *wy, image_t *du, image_t *dv, image_t *uu, image_t *vv, color_image_t *Ix, color_image_t *Iy, color_image_t *Iz, color_image_t *Ixx, color_image_t *Ixy, color_image_t *Iyy, color_image_t *Ixz, color_image_t *Iyz, const float half_delta_over3, const float half_beta, const float half_gamma_over3);
-
 #ifdef __cplusplus
 }
 #endif
